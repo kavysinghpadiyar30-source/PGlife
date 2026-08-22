04f45efc781911480efb38d45ab518641d23f1d1
kavya singh padiyar <kavysinghpadiyar30@gmail.com>
2026-08-22 17:19:41 +0530

    Fix database host name in connection script

--- includes/database_connect.php
+++ includes/database_connect.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "bxogvd5jlamdr108azjd-mysql.services.clever-cloud.com";
+$db_host = "bxogvd5jlamdrl08azjd-mysql.services.clever-cloud.com";
 $db_user = "uudflgtglaj8ynnn";
 $db_pass = "tjXUqZ4uWPGt4AkgJic5";
-$db_name = "bxogvd5jlamdr108azjd";
+$db_name = "bxogvd5jlamdrl08azjd";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
